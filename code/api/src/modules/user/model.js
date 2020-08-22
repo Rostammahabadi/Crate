d1f45c2f987011cb4574c5303239dffad939c74b
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-22 13:28:40 -0600

    Update user model attribute regarding null values

--- code/api/src/modules/user/model.js
+++ code/api/src/modules/user/model.js
@@ -27,3 +27,5 @@ module.exports = function(sequelize, DataTypes) {
 // Each key above (name, e-mail, etc.) is an attribute for the user resource. We will need to create more attributes for the user (i.e. columns in the table) for profilePicture, description, and shipping address.
 
 // The hasMany association is similar to what we would see in a Rails application. In order to track their purchases we will need to create a new resource for userProducts (still haven't decided on what exactly to name this resource, but a joins table seems like a good starting place) and create a hasMany relationship between user and products through this resources.
+
+// None of the current attributes are optional. We will need to determine whether the attributes we are adding can default to null. That process is explained here: https://sequelize.readthedocs.io/en/latest/docs/models-definition/
