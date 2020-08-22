5333011fdf91b354cad977ce6319e18f54883a97
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-22 16:01:47 -0600

    Annotate user routes

--- code/web/src/setup/routes/user.js
+++ code/web/src/setup/routes/user.js
@@ -4,6 +4,8 @@ import Signup from '../../modules/user/Signup'
 import Profile from '../../modules/user/Profile'
 import Subscriptions from '../../modules/user/Subscriptions'
 
+// Will need to also import Update 
+
 // User routes
 export default {
   login: {
