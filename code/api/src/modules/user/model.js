627f003352c2492ecc1b93ea736c2c9a5321c09c
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-22 13:09:58 -0600

    Add annotation to user model

--- code/api/src/modules/user/model.js
+++ code/api/src/modules/user/model.js
@@ -23,3 +23,7 @@ module.exports = function(sequelize, DataTypes) {
 
   return User
 }
+
+// Each key above (name, e-mail, etc.) is an attribute for the user resource. We will need to create more attributes for the user (i.e. columns in the table) for profilePicture, description, and shipping address.
+
+// The hasMany association is similar to what we would see in a Rails application. In order to track their purchases we will need to create a new resource for userProducts (still haven't decided on what exactly to name this resource, but a joins table seems like a good starting place) and create a hasMany relationship between user and products through this resources.
