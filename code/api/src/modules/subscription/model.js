4f2e987e14e7f85c2004ed2004efee20f12f382e
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-22 14:52:16 -0600

    Annotate subscription model regarding frequency

--- code/api/src/modules/subscription/model.js
+++ code/api/src/modules/subscription/model.js
@@ -18,3 +18,5 @@ module.exports = function(sequelize, DataTypes) {
 
   return Subscription
 }
+
+// All existing crates are a monthly subscription, therefore if a user signs up for a subscription (and therefore gets an order shipped to them on that day) then a future order for the same day in next month is created. I don't think we need to store this information, but can just use the most recent userProduct as a reference. 
