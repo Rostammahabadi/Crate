bf9fedb858590c2a571cfdcb2ff0c3e1c33f0e91
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-22 14:14:40 -0600

    Update user model with subscription date requirements

--- code/api/src/modules/user/model.js
+++ code/api/src/modules/user/model.js
@@ -29,3 +29,5 @@ module.exports = function(sequelize, DataTypes) {
 // The hasMany association is similar to what we would see in a Rails application. In order to track their purchases we will need to create a new resource for userProducts (still haven't decided on what exactly to name this resource, but a joins table seems like a good starting place) and create a hasMany relationship between user and products through this resources.
 
 // None of the current attributes are optional. We will need to determine whether the attributes we are adding can default to null. That process is explained here: https://sequelize.readthedocs.io/en/latest/docs/models-definition/
+
+// User subscriptions should(?) indicate how often a user recieves products, which will be how we know when the next order is coming so they can adjust it 
