fc215cedb48866dd5a9e2475bfc6723dea42217f
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-23 18:13:05 -0600

    Expand mutation annotations

--- code/api/src/modules/user/mutations.js
+++ code/api/src/modules/user/mutations.js
@@ -5,6 +5,8 @@ import { GraphQLString, GraphQLInt } from 'graphql'
 import { UserType } from './types'
 import { create, remove } from './resolvers'
 
+// This is where the data from the front end post request goes when it hits the API (if create or remove). It requires the resolvers which in turn require the `setup/models` which figures out which resources to create.
+
 // Create
 export const userSignup = {
   type: UserType,
@@ -43,4 +45,4 @@ export const userRemove = {
 
 // The attributes (args) that a user will be able to update are shipping address and email address.
 
-// In order for update to function, we will need to create an update action in the resolvers file 
+// In order for update to function, we will need to create an update action in the resolvers file
