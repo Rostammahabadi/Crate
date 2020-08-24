fa04a08a6192408447580c129af01caa949bf7b6
stellakunzang <stella.k.bonnie@gmail.com>
2020-08-23 18:13:20 -0600

    Add new annotation to subscription model

--- code/api/src/modules/subscription/model.js
+++ code/api/src/modules/subscription/model.js
@@ -11,6 +11,8 @@ module.exports = function(sequelize, DataTypes) {
     }
   })
 
+  // for user to update delivery date will need to add column to this table with next delivery date 
+
   Subscription.associate = function(models) {
     Subscription.belongsTo(models.User)
     Subscription.belongsTo(models.Crate)
@@ -19,4 +21,4 @@ module.exports = function(sequelize, DataTypes) {
   return Subscription
 }
 
-// All existing crates are a monthly subscription, therefore if a user signs up for a subscription (and therefore gets an order shipped to them on that day) then a future order for the same day in next month is created. I don't think we need to store this information, but can just use the most recent userProduct as a reference. 
+// All existing crates are a monthly subscription, therefore if a user signs up for a subscription (and therefore gets an order shipped to them on that day) then a future order for the same day in next month is created. I don't think we need to store this information, but can just use the most recent userProduct as a reference.
