ace4583f0afbedf3c00559ff951bf6246f18ce30
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-08-25 17:10:24 -0600

    add query for description, address, and image

--- code/api/src/modules/user/query.js
+++ code/api/src/modules/user/query.js
@@ -1,49 +1,108 @@
 // Imports
-import { GraphQLInt, GraphQLString, GraphQLList } from 'graphql'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 // App Imports
-import { UserType, UserLoginType, UserGenderType } from './types'
-import { getAll, getById, login, getGenders } from './resolvers'
+import serverConfig from '../../config/server'
+import params from '../../config/params'
+import models from '../../setup/models'
 
-// All
-export const users = {
-  type: new GraphQLList(UserType),
-  resolve: getAll
+// Create
+export async function create(parentValue, { name, email, password }) {
+  // Users exists with same email check
+  const user = await models.User.findOne({ where: { email } })
+
+  if (!user) {
+    // User does not exists
+    const passwordHashed = await bcrypt.hash(password, serverConfig.saltRounds)
+
+    return await models.User.create({
+      name,
+      email,
+      password: passwordHashed
+    })
+  } else {
+    // User exists
+    throw new Error(`The email ${ email } is already registered. Please try to login.`)
+  }
 }
 
-// By ID
-export const user = {
-  type: UserType,
-  args: {
-    id: { type: GraphQLInt }
-  },
-  resolve: getById
-}
+export async function login(parentValue, { email, password }) {
+  const user = await models.User.findOne({ where: { email } })
 
-// Auth
-export const userLogin = {
-  type: UserLoginType,
-  args: {
-    email: {
-      name: 'email',
-      type: GraphQLString
-    },
+  if (!user) {
+    // User does not exists
+    throw new Error(`We do not have any user registered with ${ email } email address. Please signup.`)
+  } else {
+    const userDetails = user.get()
 
-    password: {
-      name: 'password',
-      type: GraphQLString
-    },
+    // User exists
+    const passwordMatch = await bcrypt.compare(password, userDetails.password)
 
-    role: {
-      name: 'role',
-      type: GraphQLString
+    if (!passwordMatch) {
+      // Incorrect password
+      throw new Error(`Sorry, the password you entered is incorrect. Please try again.`)
+    } else {
+      const userDetailsToken = {
+        id: userDetails.id,
+        name: userDetails.name,
+        email: userDetails.email,
+        role: userDetails.role
+      }
+
+      return {
+        user: userDetails,
+        token: jwt.sign(userDetailsToken, serverConfig.secret)
+      }
     }
-  },
-  resolve: login
+  }
 }
 
-// Genders
-export const userGenders = {
-  type: new GraphQLList(UserGenderType),
-  resolve: getGenders
+// Update Email
+export async function updateEmail(parentValue, { id, email }, { auth }) {
+  if(auth.user){
+    return await models.User.update(
+      {
+      email
+    },
+      { where: { id } }
+    )
+  } else {
+    throw new Error('Operation denied.')
+  }
+}
+
+// Update address
+export async function updateAddress(parentValue, { id, address }, { auth }) {
+  if(auth.user){
+    return await models.User.update(
+      {
+        address
+      },
+      { where: { id } }
+    )
+  } else {
+    throw new Error('Operation denied.')
+  }
+}
+
+//Get by ID
+export async function getById(parentValue, { id }) {
+  return await models.User.findOne({ where: { id } })
+}
+
+
+// Get all
+export async function getAll() {
+  return await models.User.findAll()
+}
+
+// Delete
+export async function remove(parentValue, { id }) {
+  return await models.User.destroy({ where: { id } })
+}
+
+// User genders
+export async function getGenders() {
+  return Object.values(params.user.gender)
 }
